1d2e2c887f9639d3f25fdc715d9c0f309fc2c766
sedhu <sedhu@madebyfire.com>
2018-06-01 19:22:27 +0530

    text added with store

--- crud-redux/src/PostForm.js
+++ crud-redux/src/PostForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class PostForm extends Component {
 	constructor(props) {
@@ -8,18 +9,38 @@ class PostForm extends Component {
 	  	message:""
 	  };
 	  this.handleChange = this.handleChange.bind(this);
+	  this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	handleChange(e){
-		//e.preventDefault();
 		this.setState({
 			[e.target.name]: e.target.value,
 		})
 	}
+
+	handleSubmit(e){
+		e.preventDefault();
+		const { title , message } = this.state;
+		const data = {
+			id: new Date(),
+			title: title,
+			message: message
+		}
+
+		this.props.dispatch({
+	      type:'ADD_POST',
+	      data
+	  	});
+	    this.setState({
+			title:'',
+			message:''
+		})
+	}
+
 	render(){
 		return(
 			<div>
 				<h1>Creat List</h1>
-				<form className="formRow">
+				<form className="formRow" onSubmit={this.handleSubmit}>
 					<input type="text" name="title" value={this.state.title} placeholder="Enter Title" onChange={this.handleChange} />
 					<textarea name="message" value={this.state.message} placeholder="Enter Post" onChange={this.handleChange}></textarea>
 					<button>Submit</button>
@@ -29,4 +50,4 @@ class PostForm extends Component {
 	}
 }
 
-export default PostForm;
+export default connect() (PostForm);
--- crud-redux/src/index.js
+++ crud-redux/src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const store = createStore();
+import postReducer from './reducers/postReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(postReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
